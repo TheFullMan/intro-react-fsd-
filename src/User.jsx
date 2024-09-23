7a6c85d836a1f750acf01cb949b949a7d8eaadde
TheFullMan <thefullman.bx@gmail.com>
2024-09-23 15:50:49 +0200

    initial commit

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,12 @@
 const User = () => {
+
+    const myName = 'Samuel'
+    const myLastName = 'Armagnac'
+    const myAge = 27
+
     return (
       <>
-        <h1>Samuel</h1>
+        <h1>{}</h1>
         <p>Fan de gaming, je suis en train d'apprendre à coder pour pouvoir coder mon propre Jeu un jour !</p>
       </>
     );
