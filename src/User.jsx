abcade6913812e780799dc8f64a7343790c81738
TheFullMan <thefullman.bx@gmail.com>
2024-09-24 12:07:06 +0200

    first commit

--- src/User.jsx
+++ src/User.jsx
@@ -1,26 +1,12 @@
 import './User.css';
 
-const User = () => {
-
-    const info = {
-        name : 'Samuel',
-        lastName : 'Armagnac',
-        age : 27
-    }; 
-
-    const isAuthenticated = true;
-
+const User = ({name, lastname, image}) => {
+console.log(props)
     return (
-      <div className="user-container">
-        {isAuthenticated ? (
-            <h1>Welcome {info.name}!</h1>
-        ) : (
-            <h1>Please Sign In</h1>
-        )}
-        <p>
-            Fan de gaming, je suis en train d'apprendre à coder pour pouvoir coder mon propre Jeu un jour !
-        </p>
-      </div>
+      <>
+        <h1>Bonjour mon nom est {name} {lastname}</h1>
+        <img src={image} alt="image de profil"/>
+      </>
     );
   };
   
